33723a7ca9991f46bd6321a216e194d1364882ab
melia miller <meliajm@gmail.com>
2019-11-26 23:26:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,14 +38,15 @@ end
 def reduce_to_total(source_array, starting_point)
   result = 0 
   index = 0 
-  
-  if source_array[index] == starting_point
-    result += source_array[index]
-    ind = index
-  index += 1
-  else
-    result1 = source_array.sum
-  end 
+  result
+  # if source_array[index] == starting_point
+  #   result += source_array[index]
+  #   ind = index
+  # index += 1
+  # else
+  #   result1 = source_array.sum
+  # end 
+end
   # #binding.pry
   # new_arr = source_array[ind..-1]
   # result1 = 0 
@@ -55,7 +56,7 @@ def reduce_to_total(source_array, starting_point)
   # index += 1  
   # end
   # result1
-end 
+ 
 
 def reduce_to_all_true(source_array)
   index = 0 
